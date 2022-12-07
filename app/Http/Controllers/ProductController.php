7afdc64a92600807c8f129207636f77e76b32c49
Isaac  Tingo <newtone480@gmail.com>
2022-12-07 09:49:27 +0300

    Sanctum not working

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,28 +10,40 @@ class ProductController extends Controller
 
     public function index()
     {
-    return Product::all();
-        
+        return Product::all();
+
     }
 
     public function store(Request $request)
-    { 
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'price' => 'required'
+        ]);
         return Product::create($request->all());
     }
 
 
     public function show($id)
     {
-        
+        return Product::find($id);
     }
 
     public function update(Request $request, $id)
     {
-        
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $product;
     }
 
     public function destroy($id)
     {
-        
+        return product::destroy(($id));
+    }
+
+    public function search($name)
+    {
+        return product::where('name', 'like', '%' . $name . '%')->get();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,14 @@
 <?php
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index']);
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::group(['middlware' => ['auth:sanctum']], function () {
+    Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-Route::post('/products', [ProductController::class, 'store']);
+    Route::resource('products', ProductController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('/users', [UserController::class, 'index']);
+
 });
